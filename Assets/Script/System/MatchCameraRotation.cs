520da8fe21db11a102b2005824ee64cc4c972f6a
Nguyen Dang Hoang Khang <khangnguyen22072006@gmail.com>
2026-06-17 21:25:27 +0700

    Chuyển qua ik cho left hand

---
+++ Assets/Script/System/MatchCameraRotation.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class MatchCameraRotation : MonoBehaviour
+{
+    private Transform _mainCamera;
+
+    void Start()
+    {
+        // Tự động tìm Main Camera trong Scene
+        if (Camera.main != null)
+        {
+            _mainCamera = Camera.main.transform;
+        }
+    }
+
+    // Dùng LateUpdate để xoay tay SAU KHI Camera đã xoay xong
+    void LateUpdate()
+    {
+        if (_mainCamera != null)
+        {
+            // Ép cụm tay súng copy y hệt góc quay (lên/xuống, trái/phải) của Camera
+            transform.rotation = _mainCamera.rotation;
+        }
+    }
+}
